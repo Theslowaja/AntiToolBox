b182fa12dd00f94932c955d5dad6c0989c030c1e
Theslowaja <67841659+Theslowaja@users.noreply.github.com>
2022-02-05 13:00:28 +0700

    add main

---
+++ src/Theslowaja/AntiToolbox/Main.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Theslowaja\AntiToolxok;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerLoginEvent;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+class AntiCheatListener implements Listener{
+
+
+    public function onLogin(PlayerLoginEvent $event){
+        $player = $event->getPlayer();
+        $extradata = $player->getPlayerInfo()->getExtraData();
+        $os = (int)$extradata["DeviceOS"];
+        $model = (string)$extradata["DeviceModel"];
+        if($os == 1){
+            $devicemodel = explode(" ", $model);
+            if(isset($devicemodel[0])){
+                $model = strtoupper($devicemodel[0]);
+                if($model !== $devicemodel[0]){
+                    foreach (Server::getInstance()->getOnlinePlayers() as $p) {
+                        if($player->hasPermission("antitoolbox.log")){
+                            $p->sendMessage(TextFormat::RED . "STAFF > " . TextFormat::WHITE . $player->getName() . " Detected as Toolbox");
+                        }
+                        $event->getPlayer()->kick("Toolbox is not allowed");
+                    }
+                }
+
+            }
+        }
+    }
+}
